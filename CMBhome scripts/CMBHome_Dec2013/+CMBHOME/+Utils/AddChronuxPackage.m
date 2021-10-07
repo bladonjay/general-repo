40a96a7b1d9bc22c05892b9d07348a6ff5704ab2
Jay Bladon <bladonjay@gmail.com>
2021-10-07 09:09:20 -0400

    took CMBhome out
    CMBhome scripts were interfering with general matlab functionality

--- CMBhome scripts/CMBHome_Dec2013/+CMBHOME/+Utils/AddChronuxPackage.m
+++
@@ -1,7 +0,0 @@
-function AddChronuxPackage
-
-if ~isempty(findstr('chronux', path)), return; end % check that it isnt already there
-
-a = what('CMBHOME');
-
-addpath(path, genpath(fullfile(a.path(1:end-8), 'chronux'))); % adds chronux package
--- CMBhome scripts/CMBHome_Dec2013/+CMBHOME/@Session/plot_head_dir.m
+++
@@ -1,5 +0,0 @@
-function plot_head_dir(self)
-
-    line(self.ts, self.headdir, 'Color', 'k');
-end
-
