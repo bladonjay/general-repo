81864e9348d1e3409b884f5922bc9fbd6763e49b
Jay Bladon <bladonjay@gmail.com>
2021-11-03 11:10:18 -0400

    Update SmoothMat2.m

--- SmoothMat2.m
+++ SmoothMat2.m
@@ -19,14 +19,24 @@ end
 
 if std == 0, return; end
 
-[Xgrid,Ygrid]=meshgrid(-kernel_size(1)/2: kernel_size(1)/2, -kernel_size(2)/2:kernel_size(2)/2);
+% its smart to force both dimensions to be odd.
+[Xgrid,Ygrid]=meshgrid(floor(-kernel_size(1)/2): ceil(kernel_size(1)/2),...
+    floor(-kernel_size(2)/2):ceil(kernel_size(2)/2));
 
-Rgrid=sqrt((Xgrid.^2+Ygrid.^2));
+if length(std)==1
+    
+    Rgrid=sqrt((Xgrid.^2+Ygrid.^2));
+    
+    kernel = pdf('Normal', Rgrid, 0, std);
+    
+    kernel = kernel./sum(sum(kernel));
+    
+elseif length(std)==2
 
-kernel = pdf('Normal', Rgrid, 0, std);
+    Rgrid=sqrt(((Xgrid./std(1)).^2+(Ygrid./std(2)).^2));
 
-kernel = kernel./sum(sum(kernel));
-
+    kernel = pdf('Normal', Rgrid, 0, 1);
+end
 mat = nanconv(mat, kernel, 'same','edge','nanout',nanout);
 end
 
